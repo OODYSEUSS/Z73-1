b1dd7229474e214ba18871f131957420c340682e
Dmitry Demidovich <dmiry@MacBook-Air-Dmitry.local>
2023-09-13 17:01:53 +0300

    commit.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:z73_1/ui/pages/home/home_page.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
